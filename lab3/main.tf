24f346a9c3ea4aff617cacdd08ffdef59cd5ddd3
VovaZubiak <150711972+VovaZubiak@users.noreply.github.com>
2025-11-30 23:59:01 +0200

    update

--- lab3/main.tf
+++ lab3/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_managed_disk" "disk1" {
   location             = azurerm_resource_group.rg3.location
   resource_group_name  = azurerm_resource_group.rg3.name
   
-  storage_account_type = "Standard_LRS" # Standard HDD
+  storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = 32
 }
@@ -88,7 +88,7 @@ resource "azurerm_managed_disk" "disk4" {
   location             = azurerm_resource_group.rg3.location
   resource_group_name  = azurerm_resource_group.rg3.name
   
-  storage_account_type = "Standard_LRS" # Standard HDD
+  storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = 32
 }
